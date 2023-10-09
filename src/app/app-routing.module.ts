acedb5592eed71534e4d58b67086d4522e7685b8
solebellsBEACH <lucassxxavier@gmail.com>
2023-10-08 22:55:13 -0300

    feat: adding country page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CountryPageComponent } from './country-page/country-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'country/:name', component: CountryPageComponent },
   { path: 'home', component: HomeComponent }
 ];
 
